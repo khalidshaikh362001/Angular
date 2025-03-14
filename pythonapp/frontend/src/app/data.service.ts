c8dfffa4c8a98115f81845e244c1a3cc36b17b1a
Khalid <khalidraza362001@gmail.com>
2025-03-14 15:43:16 +0530

    few changes

--- pythonapp/frontend/src/app/data.service.ts
+++ pythonapp/frontend/src/app/data.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  private apiUrl = 'http://localhost:8000/api/items';
+  private apiUrl = environment.apiUrl;
 
   constructor(private http: HttpClient) { }
 
